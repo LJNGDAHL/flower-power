c578821e0f4343acfbfac9baf2eb8340300fc545
Katarina Ljungdahl <katarina@folkungagatan54.se>
2018-04-30 21:21:47 +0200

    Intial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var http = require('http')
+var tessel = require('tessel')
+var notify = require('./notify')
+
+var gpio = tessel.port['GPIO']
+var input = gpio.pin['A1']
+var power = gpio.pin['G1']
+var speaker = gpio.pin['G4']
+
+setInterval(poll, 1000 * 60 * 60)
+
+var server = http.createServer(function (req, res) {
+  try {
+    read(function (value) {
+      res.statusCode = 200
+      res.end(value)
+    })
+  } catch (err) {
+    res.statusCode = 500
+    res.end(err.message)
+  }
+})
+
+server.listen(8080)
+
+function read (cb) {
+  power.output(1)
+
+  setTimeout(function () {
+    var value = input.read()
+    power.output(0)
+    cb(value)
+  }, 10)
+}
+
+function poll () {
+  read(function (value) {
+    if (value <= 0.2) {
+      notify(speaker)
+    }
+  })
+}
---
+++ notify.js
@@ -0,0 +1,50 @@
+var series = require('async/series')
+var tessel = require('tessel')
+var gpio = tessel.port['GPIO']
+
+var FREQUENCY = 500
+var CYCLE = 0.7
+
+module.exports = notify
+
+function notify (speaker) {
+  series([
+    beep(70),
+    wait(60),
+    beep(70),
+    wait(60),
+    beep(70),
+    wait(120),
+
+    beep(180),
+    wait(60),
+    beep(180),
+    wait(60),
+    beep(180),
+    wait(120),
+
+    beep(70),
+    wait(60),
+    beep(70),
+    wait(60),
+    beep(70)
+  ])
+
+  function beep (delay) {
+    return function (cb) {
+      gpio.pwmFrequency(FREQUENCY)
+      speaker.pwmDutyCycle(CYCLE)
+
+      setTimeout(function () {
+        speaker.pwmDutyCycle(0)
+        cb()
+      }, delay)
+    }
+  }
+
+  function wait (delay) {
+    return function (cb) {
+      setTimeout(cb, delay)
+    }
+  }
+}
